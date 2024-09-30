088959f98fdfe3a5512559589565c51a1203a20d
Ricardo Antonio Gómez Sotomayor <ricardo.gomez@tecsup.edu.pe>
2024-09-30 18:37:44 -0500

    Agregar plugin kotlin-kapt y dependencias de Room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
@@ -49,8 +50,12 @@ android {
     }
 }
 
+val room_version = "2.6.1"
 dependencies {
 
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
